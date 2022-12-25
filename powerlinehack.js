6236d97c34a8ea151b3b89cac8f23a6decb6d071
FreeThrill <95888309+FreeThrill@users.noreply.github.com>
2022-12-25 15:51:25 -0600

    Update powerlinehack.js

--- powerlinehack.js
+++ powerlinehack.js
@@ -789,7 +789,7 @@
                         ? ((e = snake.x),
                           (f = snake.y),
                           (g = db),
-                          (var last_dir = a.direction),
+                          (last_dir = a.direction),
                           38 == d.keyCode || 87 == d.keyCode
                               ? 0 != a.direction && ((a.direction = 1), a.turn(a.direction, e, f, g, 0, last_dir))
                               : 37 == d.keyCode || 65 == d.keyCode
